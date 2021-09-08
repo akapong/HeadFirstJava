08208af947e35a0b21200f4ccb74bd50b6421e88
akapong <41426546+akapong@users.noreply.github.com>
2021-09-08 15:13:23 +0700

    Update App.java

--- chap1/Iftest/src/App.java
+++ chap1/Iftest/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int x = 3;
-        if (x == 3){
-            System.out.println("x == 3");
+        int x = 4;
+        if (x == 4){
+            System.out.println("x == 4");
         }
         System.out.println("Hello, World!");
     }
