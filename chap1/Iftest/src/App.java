061cdc1256e1bcde95dd35ca0cd6eb7d1c24a7a4
Akapong Chaiyapak <akapong.ov73@gmail.com>
2021-09-08 14:38:21 +0700

    test1

--- chap1/Iftest/src/App.java
+++ chap1/Iftest/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        int x = 3;
+        if (x == 3){
+            System.out.println("x == 3");
+        }
         System.out.println("Hello, World!");
     }
 }
