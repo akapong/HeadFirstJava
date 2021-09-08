1ed59684d0f63d8b287ddacc2c6056debc7b342a
Akapong Chaiyapak <akapong.ov73@gmail.com>
2021-09-08 14:12:12 +0700

    test ignore

---
+++ chap1/Iftest/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 2;
+        if(x == 3){
+            System.out.println("x must be 3");
+        } else{
+            System.out.println("x is not 3");
+        }
+        //test test test
+        //test test
+        System.out.println("This runs no matter what");
+    }
+}
