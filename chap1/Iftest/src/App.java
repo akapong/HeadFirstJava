e23eee48e16dfbf809c32be99a87498688022829
Akapong Chaiyapak <akapong.ov73@gmail.com>
2021-09-10 13:55:21 +0700

    testing 12

--- chap1/Iftest/src/App.java
+++ chap1/Iftest/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int x = 11;
-        if (x == 11){
-            System.out.println("x == 11");
+        int x = 12;
+        if (x == 12){
+            System.out.println("x == 12");
         }
         System.out.println("Hello, World!");
     }
