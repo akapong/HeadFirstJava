b3a612020122bdb5edd367bb5b187d53d5678520
Akapong Chaiyapak <akapong.ov73@gmail.com>
2021-09-10 13:49:40 +0700

    made other changes

--- chap1/Iftest/src/App.java
+++ chap1/Iftest/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int x = 10;
-        if (x == 10){
-            System.out.println("x == 10");
+        int x = 20;
+        if (x == 20){
+            System.out.println("x == 20");
         }
         System.out.println("Hello, World!");
     }
